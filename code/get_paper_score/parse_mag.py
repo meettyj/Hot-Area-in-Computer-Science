ff4635b8eb58432aa6ecfca9be3b22008b22839f
Fan Xie <fx349@nyu.edu>
2019-07-27 23:28:06 -0700

    modify

---
+++ code/get_paper_score/parse_mag.py
@@ -0,0 +1,57 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+This is an example implementation of PageRank. For more conventional use,
+Please refer to PageRank implementation provided by graphx
+
+Example Usage:
+bin/spark-submit examples/src/main/python/pagerank.py data/mllib/pagerank_data.txt 10
+"""
+from __future__ import print_function
+
+import sys
+
+from pyspark.sql import SparkSession
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: parse_mag <file> <output_file>", file=sys.stderr)
+        sys.exit(-1)
+
+
+    # Initialize the spark context.
+    spark = SparkSession \
+        .builder \
+        .appName("ParseArxivDataset") \
+        .getOrCreate()
+
+    # Loads in input file. It should be in format of:
+    #     Json record
+
+    df = spark.read.json(sys.argv[1])
+
+    df.printSchema()
+
+    #parse_record = df.select(df['title'], df['categories'])
+
+    #parse_record.show()
+
+    #parse_record.write.json(sys.argv[2])
+
+    spark.stop()
